0baaabdf1cb0a7a5b5e3cf0f1546a812ebc9ad2e
Jeffreyp1 <carrjeffrey12270@gmail.com>
2025-03-17 23:33:59 -0700

    Modified Redis settings

--- trading-service/pkg/redisClient/redisClient.go
+++ trading-service/pkg/redisClient/redisClient.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -13,7 +14,10 @@ var Client *redis.Client
 
 func InitRedis() {
 	Client = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:         "localhost:6379",
+		PoolSize:     10, // Increase this based on your system's capabilities
+		MinIdleConns: 5,  // Keep some connections ready
+		PoolTimeout:  time.Second * 3,
 	})
 	_, err := Client.Ping(Ctx).Result()
 	if err != nil {
