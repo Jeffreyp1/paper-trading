54fac388371d3e79aa4a795fa3b2f54f2826f11a
Jeffreyp1 <carrjeffrey12270@gmail.com>
2025-03-18 13:42:59 -0700

    Removed HandleTrade function to accomodate for worker pool

--- trading-service/services/trade/trade_service.go
+++ trading-service/services/trade/trade_service.go
@@ -5,12 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
-	"strconv"
-	"strings"
 
 	"trading-service/pkg/redisClient"
-	redisStorage "trading-service/redis"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -30,16 +26,12 @@ type TradeRequest struct {
 	} `json:"stock"`
 }
 
-func executeBuy(ctx context.Context, trade TradeRequest, balance float64, totalCost float64) {
-	// ✅ Convert stock slice to JSON
+func ExecuteBuy(ctx context.Context, trade TradeRequest, balance float64, totalCost float64) {
 	stockJSON, err := json.Marshal(trade.Stock)
 	if err != nil {
 		log.Fatalf("Failed to serialize stock data: %v", err)
 		return
 	}
-
-	// ✅ Push to Redis Stream
-	// fmt.Println(trade.Stock)
 	pipeline := redisClient.Client.TxPipeline()
 	pipeline.HSet(ctx, "user_balance", fmt.Sprint(trade.UserID), balance-totalCost)
 	stock, err := pipeline.XAdd(ctx, &redis.XAddArgs{
@@ -64,48 +56,3 @@ func executeBuy(ctx context.Context, trade TradeRequest, balance float64, totalC
 	}
 	log.Println("✅ Trade successfully added to Redis Stream!")
 }
-
-func HandleTrade(response http.ResponseWriter, request *http.Request) {
-	ctx := context.Background()
-	var tradeData TradeRequest
-	defer request.Body.Close()
-	err := json.NewDecoder(request.Body).Decode(&tradeData)
-	if err != nil {
-		http.Error(response, "Invalid request", http.StatusBadRequest)
-		return
-	}
-	userId := tradeData.UserID
-	if strings.ToLower((tradeData.Action)) == "buy" {
-		balanceStr, err := redisClient.Client.HGet(ctx, "user_balance", fmt.Sprint(userId)).Result()
-		balance, err := strconv.ParseFloat(balanceStr, 64)
-		if err != nil {
-			http.Error(response, "User not found or balance", http.StatusBadRequest)
-		}
-		var totalCost float64
-		for i, stock := range tradeData.Stock {
-			stockPrice, err := redisStorage.GetStockPrice(stock.Symbol)
-			if err != nil {
-				log.Printf("Failed to fetch price")
-			} else {
-				tradeData.Stock[i].Price = stockPrice.Price
-			}
-			totalCost += stockPrice.Price * stock.Quantity
-		}
-		if totalCost < balance {
-			executeBuy(ctx, tradeData, balance, totalCost)
-			// if success != nil {
-			// 	fmt.Println("FAILED TO RUN ", success)
-			// 	return
-			// }
-		} else {
-			http.Error(response, "Insufficient funds", http.StatusForbidden)
-			return
-		}
-
-	}
-	response.WriteHeader(http.StatusOK)
-	response.Write([]byte(`{"message": "Trade executed successfully"}`))
-
-}
-
-//
