86cb196c149b523823f7e2ca97de981f5d0e5e13
Jeffreyp1 <carrjeffrey12270@gmail.com>
2025-04-01 00:07:26 -0700

    Added upsert sql batching

--- trading-service/services/workers/kafkaToSql.go
+++ trading-service/services/workers/kafkaToSql.go
@@ -1,13 +1,15 @@
 package workers
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
-	"github.com/segmentio/kafka-go"
 )
 
 type Trade struct {
@@ -22,11 +24,222 @@ type Trade struct {
 }
 
 var (
-	batchSize     = 10000
+	batchSize     = 1000
 	batchTimeout  = 1 * time.Second
 	consumerTopic = "trade_events"
 )
 
+func processKafka(workerId int, db *sql.DB) error {
+	r, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "postgres-writer",
+		// "auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create Kafka Consumer: %v", err)
+	}
+	err = r.SubscribeTopics([]string{consumerTopic}, nil)
+	if err != nil {
+		log.Fatalf("Subscription Error: %v", err)
+	}
+	var batch []Trade
+	ticker := time.NewTicker(batchTimeout)
+	defer ticker.Stop()
+	// Before the for loop:
+	seenUsers := make(map[int]bool)
+
+	for {
+		event, err := r.ReadMessage(100 * time.Millisecond)
+		if err == nil {
+			var t Trade
+			if err := json.Unmarshal(event.Value, &t); err != nil {
+				continue
+			}
+			if seenUsers[t.UserID] {
+				log.Printf("Skipping user %d (already in batch)", t.UserID)
+				continue
+			}
+			seenUsers[t.UserID] = true
+			batch = append(batch, t)
+
+			if len(batch) >= batchSize {
+				insertBatchToPostgres(db, batch)
+				batch = nil
+				seenUsers = make(map[int]bool)
+			}
+		}
+
+		// flush once per second, only if there's something
+		select {
+		case <-ticker.C:
+			if len(batch) > 0 {
+				insertBatchToPostgres(db, batch)
+				batch = nil
+				seenUsers = make(map[int]bool)
+			}
+		default:
+			// nothing to do
+		}
+	}
+
+	// for {
+	// 	select {
+	// 	case <-ticker.C:
+	// 		if len(batch) > 0 {
+	// 			insertBatchToPostgres(db, batch)
+	// 			batch = nil
+	// 		}
+	// 	default:
+	// 		event, err := r.ReadMessage(10 * time.Millisecond)
+	// 		if err != nil {
+	// 			continue
+	// 		}
+	// 		var trade Trade
+	// 		// log.Printf("🔥 RAW Kafka payload: %s", string(event.Value))
+	// 		err = json.Unmarshal(event.Value, &trade)
+	// 		if err != nil {
+	// 			log.Println("Invalid Trade Payload", err)
+	// 			continue
+	// 		}
+
+	// 		batch = append(batch, trade)
+
+	// 		if len(batch) >= batchSize {
+	// 			err := insertBatchToPostgres(db, batch)
+	// 			if err != nil {
+	// 				log.Printf("Failed to insert Batch %v, err")
+	// 			}
+	// 			batch = nil
+	// 		}
+
+	// 	}
+
+	// }
+}
+func upsertBalancePositionsAndTradeHistory(db *sql.DB, trades []Trade) error {
+	fmt.Printf("Starting")
+	if len(trades) == 0 {
+		return nil
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	var insert_trades []string
+	var positions []string
+	var args []interface{}
+	var (
+		statement    strings.Builder
+		whereIn      []string
+		balance_args []interface{}
+	)
+	for i, trade := range trades {
+		pos := i*2 + 1
+		statement.WriteString(fmt.Sprintf("When id = $%d THEN $%d ", pos, pos+1))
+		whereIn = append(whereIn, fmt.Sprintf("$%d", pos))
+		balance_args = append(balance_args, trade.UserID, trade.Balance)
+		for _, stock := range trade.Stocks {
+			pos = len(args) + 1
+			insert_trades = append(insert_trades, fmt.Sprintf(" ($%d, $%d, $%d, $%d, 'BUY') ", pos, pos+1, pos+2, pos+3))
+			positions = append(positions, fmt.Sprintf("($%d, $%d, $%d, $%d)", pos, pos+1, pos+2, pos+3))
+			args = append(args, trade.UserID, stock.Symbol, stock.Quantity, stock.Price)
+		}
+	}
+	if len(insert_trades) == 0 || len(positions) == 0 {
+		return fmt.Errorf("no valid inserts/upserts")
+	}
+	final_trades := fmt.Sprintf(`
+		INSERT INTO trades (user_id, symbol, quantity, executed_price, trade_type)
+		VALUES %s`, strings.Join(insert_trades, ", "))
+
+	insert_update_positions := fmt.Sprintf(
+		`Insert INTO positions (user_id, symbol, quantity, average_price)
+		VALUES %s
+		ON CONFLICT(user_id, symbol)
+		DO UPDATE SET
+			quantity = positions.quantity + EXCLUDED.quantity,
+			average_price = ((positions.quantity * positions.average_price) + (EXCLUDED.quantity * EXCLUDED.average_price))/ (positions.quantity + EXCLUDED.quantity),
+			updated_at = CURRENT_TIMESTAMP;`, strings.Join(positions, ", "))
+	query := fmt.Sprintf(`
+			UPDATE users
+			SET balance = CASE
+				%s
+				ELSE balance
+			END
+			WHERE id IN (%s)
+		`, statement.String(), strings.Join(whereIn, ","))
+	_, err = tx.ExecContext(ctx, final_trades, args...)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	_, err = tx.ExecContext(ctx, insert_update_positions, args...)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	_, err = tx.ExecContext(ctx, query, balance_args...)
+	if err != nil {
+		tx.Rollback()
+		fmt.Printf("Failed to insert/update positions: %v\n", err) // Log exact failure
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		log.Printf("Transaction cimmit failed: %v\n", err)
+		return err
+	}
+	return nil
+}
+func insertBatchToPostgres(db *sql.DB, trades []Trade) error {
+	err := upsertBalancePositionsAndTradeHistory(db, trades)
+	if err != nil {
+		log.Printf("Error upserting balance, positions, and trade history %v", err)
+		return err
+	}
+	return nil
+
+}
+func StartKafkaConsumer(workerCount int, db *sql.DB) {
+	for i := 1; i <= workerCount; i++ {
+		go func(id int) {
+			processKafka(id, db)
+		}(i)
+	}
+}
+
+// func updateBalances(db *sql.DB, trades []Trade) error {
+// 	if len(trades) == 0 {
+// 		return nil
+// 	}
+// 	var (
+// 		statement strings.Builder
+// 		whereIn   []string
+// 		args      []interface{}
+// 	)
+// 	for i, trade := range trades {
+// 		pos := i*2 + 1
+// 		statement.WriteString(fmt.Sprintf("When id = $%d THEN $%d ", pos, pos+1))
+// 		whereIn = append(whereIn, fmt.Sprintf("$%d", pos))
+// 		args = append(args, trade.UserID, trade.Balance)
+
+// 	}
+
+// 	query := fmt.Sprintf(`
+// 		UPDATE users
+// 		SET balance = CASE
+// 			%s
+// 			ELSE balance
+// 		END
+// 		WHERE id IN (%s)
+// 	`, statement.String(), strings.Join(whereIn, ","))
+
+// 	_, err := db.Exec(query, args...)
+// 	return err
+// }
 // func insertTradeToPostgres(ctx context.Context, db *sql.DB, trade Trade) error {
 // 	tx, err := db.BeginTx(ctx, nil)
 // 	trade.Balance
@@ -73,73 +286,3 @@ var (
 //		}
 //		return nil
 //	}
-func processKafka(workerId int, db *sql.DB) error {
-	r, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost:9092",
-		"group.id":          "postgres-writer",
-		"auto.offset.reset": "earliest",
-	})
-	if err != nil {
-		log.Fatalf("Failed to create Kafka Consumer: %v", err)
-	}
-	err = r.SubscribeTopics([]string{consumerTopic}, nil)
-	if err != nil {
-		log.Fatalf("Subscription Error: %v", err)
-	}
-	var batch []Trade
-	ticker := time.NewTicker(batchTimeout)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			if len(batch) > 0 {
-				insertBatchToPostgres(db, batch)
-				batch = nil
-			}
-		default:
-			event, err := r.ReadMessage(100 * time.Millisecond)
-			if err != nil {
-				continue
-			}
-			var trade Trade
-			err = json.Unmarshal(event.Value, &trade)
-			if err != nil {
-				log.Println("Invalid Trade Payload", err)
-				continue
-			}
-
-			batch = append(batch, trade)
-
-			if len(batch) >= batchSize {
-				insertBatchToPostgres(db, batch)
-				batch = nil
-			}
-
-		}
-		// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		// defer cancel()
-
-		// message, err := r.ReadMessage(ctx)
-		// if err != nil {
-		// 	log.Printf("Error reading message %v", err)
-		// 	continue
-		// }
-
-		// var trade Trade
-		// err = json.Unmarshal(message.Value, &trade)
-		// if err != nil {
-		// 	log.Printf("Failed to unmarshal message.Value %v", err)
-		// 	continue
-		// }
-
-		// err = insertTradeToPostgres(ctx, db, trade)
-
-	}
-	return nil
-}
-func insertBatchToPostgres(db *sql.DB, trades []Trade) {
-	updateBalances(db, trades)
-	upsertPositions(db, trades)
-	insertTradeHistory(db, trades)
-
-}
